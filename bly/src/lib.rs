de07190238daded35102a510b584b6bc1913c3c4
Lattexshz <lattex.code@gmail.com>
2023-02-24 22:40:50 +0900

    Enabled to draw rounded rectangles in Direct2D.

--- bly/src/lib.rs
+++ bly/src/lib.rs
@@ -82,6 +82,20 @@ impl Bdc {
         }
     }
 
+    pub fn draw_rounded_rect(&mut self,
+        x: f32,
+        y: f32,
+        width: f32,
+        height: f32,
+        radius_x:f32,
+        radius_y:f32,
+        color: Color) {
+            let vec:Vec4 = color.into();
+            unsafe {
+                self.backend.draw_rounded_rect(x,y,width,height,radius_x,radius_y,vec.0 as f32,vec.1 as f32,vec.2 as f32,vec.3 as f32);
+            }
+        }
+
     /// Draws a line
     pub fn draw_line(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, stroke: f32, color: Color) {
         unsafe {
