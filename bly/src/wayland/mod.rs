d29b7e47d69d2796d859dd4c02825a37a9da6387
Lattexshz <125027502+Lattexshz@users.noreply.github.com>
2023-03-02 09:58:06 +0900

    Fix wayland initialziation

--- bly/src/wayland/mod.rs
+++ bly/src/wayland/mod.rs
@@ -1,57 +1,74 @@
 use std::ffi::c_void;
 use khronos_egl::Context;
 use crate::{Backend, Point2};
+use wayland_sys::client::*;
 extern crate khronos_egl as egl;
+use wayland_sys::client::WAYLAND_CLIENT_HANDLE;
+use wayland_sys::egl::WAYLAND_EGL_HANDLE;
 
 pub fn create_wayland_backend(surface: *mut c_void) -> Result<WaylandBackend, egl::Error> {
+    let wl_display = unsafe {
+        wayland_sys::ffi_dispatch!(WAYLAND_CLIENT_HANDLE, wl_display_connect, ::std::ptr::null())
+    };
     let egl = egl::Instance::new(egl::Static);
-    info!("EGL instance has been created.");
-
-    let wayland_display = wayland_client::Display::connect_to_env().expect("unable to connect to the wayland server");
-    
-    info!("Connected to Wayland server.");
-    let display = egl.get_display(wayland_display.get_display_ptr() as *mut std::ffi::c_void).unwrap();
-
+    let display = egl.get_display(wl_display as *mut std::ffi::c_void).unwrap();
     egl.initialize(display)?;
-    info!("EGL initialization succeeded");
 
     let attributes = [
         egl::RED_SIZE, 8,
         egl::GREEN_SIZE, 8,
         egl::BLUE_SIZE, 8,
+        egl::DEPTH_SIZE, 24,
+        egl::SURFACE_TYPE,egl::OPENGL_ES2_BIT,
         egl::NONE
     ];
 
     let config = egl.choose_first_config(display, &attributes)?.expect("unable to find an appropriate EGL configuration");
 
     let context_attributes = [
-        egl::CONTEXT_MAJOR_VERSION, 4,
-        egl::CONTEXT_MINOR_VERSION, 0,
-        egl::CONTEXT_OPENGL_PROFILE_MASK, egl::CONTEXT_OPENGL_CORE_PROFILE_BIT,
+		egl::CONTEXT_CLIENT_VERSION, 2,
         egl::NONE
     ];
 
+
+    let egl_window = unsafe {
+        wayland_sys::ffi_dispatch!(WAYLAND_EGL_HANDLE,wl_egl_window_create,wl_display as *mut wl_proxy,520,520)
+    };
+
+    let surface = unsafe {egl.create_window_surface(display,config,egl_window as *mut c_void,None)? };
+
     let context = egl.create_context(display, config, None, &context_attributes)?;
 
-    info!("EGL context successfully created");
-    info!("Pointer: {:?}",context);
+    egl.make_current(display,Some(surface),Some(surface),Some(context))?;
 
     Ok(WaylandBackend {
+        egl,
+        surface,
+        display,
+        wl_display,
         context
     })
 }
 
 pub struct WaylandBackend {
-    context: Context
+    egl: egl::Instance<egl::Static>,
+    surface: egl::Surface,
+    display: egl::Display,
+    wl_display: *mut wl_display,
+    context: egl::Context
 }
 
 impl Backend for WaylandBackend {
     unsafe fn begin_draw(&mut self) {
-        todo!()
+        unsafe {
+            wayland_sys::ffi_dispatch!(WAYLAND_CLIENT_HANDLE,wl_display_dispatch_pending,self.wl_display);
+        }
+        gl::ClearColor(0.5, 0.3, 0.0, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
     }
 
     unsafe fn flush(&mut self) {
-        todo!()
+        self.egl.swap_buffers(self.display,self.surface);
     }
 
     unsafe fn get_display_size(&mut self) -> (u32, u32) {
@@ -59,22 +76,57 @@ impl Backend for WaylandBackend {
     }
 
     unsafe fn clear(&mut self, r: f32, g: f32, b: f32, a: f32) {
+        gl::ClearColor(0.5, 0.3, 0.0, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+    }
+
+    unsafe fn ellipse(
+        &mut self,
+        point: Point2<f32>,
+        radius: f32,
+        r: f32,
+        g: f32,
+        b: f32,
+        a: f32,
+    ) {
         todo!()
     }
 
-    unsafe fn ellipse(&mut self, point: Point2<f32>, radius: f32, r: f32, g: f32, b: f32, a: f32) {
+    unsafe fn rectangle(
+        &mut self,
+        point1: Point2<f32>,
+        point2: Point2<f32>,
+        r: f32,
+        g: f32,
+        b: f32,
+        a: f32,
+    ) {
         todo!()
     }
 
-    unsafe fn rectangle(&mut self, point1: Point2<f32>, point2: Point2<f32>, r: f32, g: f32, b: f32, a: f32) {
+    unsafe fn rounded_rectangle(
+        &mut self,
+        point1: Point2<f32>,
+        point2: Point2<f32>,
+        radius: f32,
+        r: f32,
+        g: f32,
+        b: f32,
+        a: f32,
+    ) {
         todo!()
     }
 
-    unsafe fn rounded_rectangle(&mut self, point1: Point2<f32>, point2: Point2<f32>, radius: f32, r: f32, g: f32, b: f32, a: f32) {
-        todo!()
-    }
-
-    unsafe fn line(&mut self, point1: Point2<f32>, point2: Point2<f32>, stroke: f32, r: f32, g: f32, b: f32, a: f32) {
+    unsafe fn line(
+        &mut self,
+        point1: Point2<f32>,
+        point2: Point2<f32>,
+        stroke: f32,
+        r: f32,
+        g: f32,
+        b: f32,
+        a: f32,
+    ) {
         todo!()
     }
 }
