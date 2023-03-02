eec7f22d5faeb3eb076ffe38115284122e0cc409
Lattexshz <lattex.code@gmail.com>
2023-03-03 07:43:11 +0900

    Use of gl-generator as OpenGL binding

--- bly/src/wayland/mod.rs
+++ bly/src/wayland/mod.rs
@@ -6,8 +6,12 @@ extern crate khronos_egl as egl;
 use wayland_sys::client::WAYLAND_CLIENT_HANDLE;
 use wayland_sys::egl::WAYLAND_EGL_HANDLE;
 
+mod gl {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
 #[doc(hidden)]
-pub fn create_wayland_backend(surface: *mut c_void) -> Result<WaylandBackend, egl::Error> {
+pub fn create_wayland_backend(wl_surface: *mut c_void) -> Result<WaylandBackend, egl::Error> {
     let wl_display = unsafe {
         wayland_sys::ffi_dispatch!(WAYLAND_CLIENT_HANDLE, wl_display_connect, ::std::ptr::null())
     };
@@ -16,18 +20,19 @@ pub fn create_wayland_backend(surface: *mut c_void) -> Result<WaylandBackend, eg
     egl.initialize(display)?;
 
     let attributes = [
+        egl::SURFACE_TYPE,egl::WINDOW_BIT,
         egl::RED_SIZE, 8,
         egl::GREEN_SIZE, 8,
         egl::BLUE_SIZE, 8,
         egl::DEPTH_SIZE, 24,
-        egl::SURFACE_TYPE,egl::OPENGL_ES2_BIT,
+        egl::RENDERABLE_TYPE, egl::OPENGL_ES2_BIT,
         egl::NONE
     ];
 
     let config = egl.choose_first_config(display, &attributes)?.expect("unable to find an appropriate EGL configuration");
 
     let context_attributes = [
-		egl::CONTEXT_CLIENT_VERSION, 2,
+        egl::CONTEXT_CLIENT_VERSION, 2,
         egl::NONE
     ];
 
@@ -41,13 +46,27 @@ pub fn create_wayland_backend(surface: *mut c_void) -> Result<WaylandBackend, eg
     let context = egl.create_context(display, config, None, &context_attributes)?;
 
     egl.make_current(display,Some(surface),Some(surface),Some(context))?;
+    let gl = gl::load_with(|s|egl.get_proc_address(s).unwrap() as *const _);
+
+    unsafe {
+        use std::ffi::CStr;
+        use std::os::raw::c_char;
+
+        let v:*const c_char = gl::GetString(gl::VERSION) as *const i8;
+        let c_str: &CStr = unsafe { CStr::from_ptr(v) };
+        let str_: &str = c_str.to_str().unwrap();
+        info!("Using {}",str_);
+    }
+
+    egl.swap_buffers(display,surface);
+
+
 
     Ok(WaylandBackend {
         egl,
         surface,
         display,
         wl_display,
-        context
     })
 }
 
@@ -57,16 +76,15 @@ pub struct WaylandBackend {
     surface: egl::Surface,
     display: egl::Display,
     wl_display: *mut wl_display,
-    context: Context
 }
 
 impl Backend for WaylandBackend {
     unsafe fn begin_draw(&mut self) {
         unsafe {
             wayland_sys::ffi_dispatch!(WAYLAND_CLIENT_HANDLE,wl_display_dispatch_pending,self.wl_display);
+            gl::ClearColor(1.0,1.0,1.0,1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
-        gl::ClearColor(0.5, 0.3, 0.0, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT);
     }
 
     unsafe fn flush(&mut self) {
@@ -78,8 +96,7 @@ impl Backend for WaylandBackend {
     }
 
     unsafe fn clear(&mut self, r: f32, g: f32, b: f32, a: f32) {
-        gl::ClearColor(0.5, 0.3, 0.0, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT);
+
     }
 
     unsafe fn ellipse(
@@ -103,7 +120,7 @@ impl Backend for WaylandBackend {
         b: f32,
         a: f32,
     ) {
-        todo!()
+        //todo!()
     }
 
     unsafe fn rounded_rectangle(
